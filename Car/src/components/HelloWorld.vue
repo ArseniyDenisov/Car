bc0f907e4bbaf4fc8a2524fb3804ad050e5031ef
Ars20000 <nimbanan2014@mail.ru>
2024-12-29 17:26:26 +0300

    Ну чо народ погнали нахуй 1

---
+++ Car/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      <a href="https://vk.com/im">хуй</a>
+      <h1>
+
+        Добро пожаловать
+            на интернет-аукцион!
+Торги начинаются! Ваша возможность сэкономить
+
+      </h1>
+     
+    </h3>
+  </div>
+</template>
+
+
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+
+
